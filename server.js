3ee735d57838592f17e3c04f45f8f2326b48c2b3
Tom Crick <tomcrick@down-right.co.uk>
2015-05-15 09:36:41 +0100

    Finished building a RESTful api

--- server.js
+++ server.js
@@ -99,6 +99,69 @@ username already exists. '});
 		}); 
 	})
 
+		// get all the users (accessed at GET http://localhost:8080/api/users)
+		.get(function(req, res) {
+			User.find(function(err, users) { 
+				if (err) res.send(err);
+	
+				// return the users
+				res.json(users);
+
+			}); 
+	});
+
+// on routes that end in /users/:user_id
+// ------ 
+apiRouter.route('/users/:user_id')
+	// get the user with that id
+	// (accessed at GET http://localhost:8080/api/users/:user_id) 
+	.get(function(req, res) {
+		User.findById(req.params.user_id, function(err, user) { 
+			if (err) res.send(err);
+				// return that user
+				res.json(user);
+		}); 
+	})
+
+	// update the user with this id
+	// (accessed at PUT http://localhost:8080/api/users/:user_id) 
+	.put(function(req, res) {
+                
+                // use our user model to find the user we want
+				User.findById(req.params.user_id, function(err, user) { 
+
+					if (err) res.send(err);
+
+					// update the users info only if its new
+					if (req.body.name) user.name = req.body.name;
+					if (req.body.username) user.username = req.body.username; if (req.body.password) user.password = req.body.password;
+
+					// save the user
+					user.save(function(err) {
+						if (err) res.send(err);
+
+						// return a message
+						res.json({ message: 'User updated!' });
+
+					});
+
+
+				}); 
+	})
+	// delete the user with this id
+	// (accessed at DELETE http://localhost:8080/api/users/:user_id)
+	.delete(function(req, res) {
+        User.remove({
+                _id: req.params.user_id
+		}, function(err, user) {
+			if (err) return res.send(err);
+            
+            res.json({ message: 'Successfully deleted' });
+		}); 
+    });
+
+
+
 
 
 
